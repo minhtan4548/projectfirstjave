328f056494aea373f6c2c3a25db35cf247b9bf66
minhtan4548 <minhtan4548@gmail.com>
2026-05-08 10:11:44 +0700

    tandeptrai

--- src/minhtan/HelloWorld.java
+++ src/minhtan/HelloWorld.java
@@ -1,12 +1,15 @@
 package minhtan;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-
-		System.out.println("Hello World");
-		System.out.println("Hello World");
-		System.out.println("Hello World");
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Fist number: ");
+		int firstnumber = scanner.nextInt();
+		System.out.println("Second number: ");
+		int secondnumber = scanner.nextInt();
+		System.out.println("Min =  " + Math.min(firstnumber, secondnumber));
 
 	}
-
 }
